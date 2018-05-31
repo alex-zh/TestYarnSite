5b2ac8c526a358f2b93619a594daa1cfad13203c
alexander zh <alexander.zhigalov@gmail.com>
2018-05-31 08:48:04 +0300

    Add rating starts support

--- itemTemplate.js
+++ itemTemplate.js
@@ -1,3 +1,13 @@
+function ratingStarsTemplate(fullStartsNumber) {
+    var startsString = "";
+    for (var i = 1; i < 6; i++) {
+        var className = (i <= fullStartsNumber) ? 'full-star' : 'empty-star'
+        startsString += `<span id="${i}" class="rating-star ${className}"></span>`
+    }
+
+    return startsString;
+}
+
 function template(item) {
     return `<div class="item" id="item_${item.id}">
     <div class="item__image-container">
@@ -7,9 +17,19 @@ function template(item) {
     </div>
     <div class="item__content-container">
         <div>
-            <div class="content-container__name">
-                <h4>${item.name}</h4>
-                <div  class="content-container__show-details">Показать описание</div>
+            <div class="content-container__header">
+                <div class="name-stars">
+                    <h4>${item.name}</h4>
+                    <div class="name-stars__rating">
+                    <div class="rating-box">
+                        ${ratingStarsTemplate(item.stars)}                        
+                     </div>
+      
+                    </div>
+                </div> 
+                <div  class="content-container__show-details">
+                    Показать описание
+                </div>
             </div>
             <div class="details-container">
                 <div class="details-container__info">
@@ -25,7 +45,8 @@ function template(item) {
                         <span>Вес:</span>
                         <span>${item.weight}</span>
                     </div>
-                    <div class="details-container__full-description">                        
+                    <div class="details-container__full-description">
+                      ${item.getDescription()}                        
                     </div>
                 </div>
                 <div class="details-container__sales-block">
--- main.js
+++ main.js
@@ -5,28 +5,61 @@ function YarnItem(jsonItem) {
     )
 
     this.getDescription = function () {
-        return this.description;
+        if(this.showDescription)
+            return this.description;
+        else
+            return ''
     }
 }
 
-
-var yarnItems = json_array.map(jsonItem => {
-    return new YarnItem(jsonItem);
+document.getElementById('refreshButton').addEventListener('click', function () {
+    load()
+    render()
 })
 
-var listContainer = document.getElementsByClassName("list-container__list")[0];
+var yarnItems = []
 
-yarnItems.forEach(item => {
-    var itemHtml = template(item);
-    var li = document.createElement('li');
-    li.innerHTML = itemHtml;
-    listContainer.appendChild(li);
-
-    var button = document.querySelector(`#item_${item.id} .content-container__show-details`);
-
-    button.addEventListener('click', function () {
-        var fullDescriptionDiv = document.querySelector(`#item_${item.id} .details-container__full-description`)
-        fullDescriptionDiv.innerText = item.getDescription();
+function load() {
+    yarnItems = json_array.map(jsonItem => {
+        return new YarnItem(jsonItem);
     })
-})
+}
 
+function render() {
+    var listContainer = document.getElementsByClassName("list-container__list")[0];
+    listContainer.innerHTML = '';
+
+    yarnItems.forEach(item => {
+        var itemHtml = template(item);
+        var li = document.createElement('li');
+        li.innerHTML = itemHtml;
+        listContainer.appendChild(li);
+
+        var button = document.querySelector(`#item_${item.id} .content-container__show-details`);
+
+        button.addEventListener('click', function () {
+            updateYarn(item.id, {showDescription: true})
+        })
+
+        var stars = document.querySelectorAll(`#item_${item.id} .rating-star`);
+        stars.forEach(star => {
+            star.addEventListener('click', function () {
+                updateYarn(item.id, {stars: +star.id})
+            })
+        })
+    })
+}
+
+function updateYarn(id, newState)
+{
+    var yarnItem = yarnItems.find(item=>item.id === id);
+
+    Object.keys(newState).forEach(key=>{
+        yarnItem[key] = newState[key];
+    })
+
+    render()
+}
+
+load()
+render()
